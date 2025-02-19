7f067f876868af76777c7daf612df69e6455a9cd
Robin Dehde <robin.dehde@kalasar.com>
2025-02-19 11:38:13 +0100

    made clear the ordering of the sections

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,16 @@
 
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import Hero from "@/components/sections/Hero";
-import SocialProof from "@/components/sections/SocialProof";
-import Features from "@/components/sections/Features";
-import SolutionOverview from "@/components/sections/SolutionOverview";
-import Stats from "@/components/sections/Stats";
-import HowItWorks from "@/components/sections/HowItWorks";
-import Integrations from "@/components/sections/Integrations";
-import FAQ from "@/components/sections/FAQ";
-import CallToValue from "@/components/sections/CallToValue";
-import Pricing from "@/components/sections/Pricing";
+import Hero from "@/components/sections/01_Hero";
+import SocialProof from "@/components/sections/02_SocialProof";
+import Features from "@/components/sections/03_Features";
+import SolutionOverview from "@/components/sections/04_SolutionOverview";
+import Stats from "@/components/sections/05_Stats";
+import HowItWorks from "@/components/sections/06_HowItWorks";
+import Integrations from "@/components/sections/07_Integrations";
+import FAQ from "@/components/sections/08_FAQ";
+import CallToValue from "@/components/sections/09_CallToValue";
+import Pricing from "@/components/sections/10_Pricing";
 
 const Index = () => {
   return (
